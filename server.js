cccca67084a3750cbc8234de594e47b8c3838ef6
siva_user <techiesivanarayanan@gmail.com>
2021-06-08 08:44:27 +0530

    Enabled cors

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+
 const PORT = process.env.PORT || 9000;
 
-express()
-  .use(express.static(path.join(__dirname, "dist")))
-  .get("*", (req, res) => {
+let app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app
+  .get("*", cors(), (req, res) => {
     res.sendFile("index.html", { root: "dist" });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
