1da487b58d1361ca645f46e34a9260d6c84ef9b8
siva_user <techiesivanarayanan@gmail.com>
2021-06-09 11:57:05 +0530

    Add angular app

--- src/test-root-config.js
+++ src/test-root-config.js
@@ -1,16 +1,16 @@
 import { registerApplication, start } from "single-spa";
 
 registerApplication({
-  name: "@test/mfe-react-spa",
-  app: () => System.import("@test/mfe-react-spa"),
+  name: "@test/mfe-react-app",
+  app: () => System.import("@test/mfe-react-app"),
   activeWhen: ["/react"],
 });
 
-// registerApplication({
-//   name: "@test/mfe-angular-app",
-//   app: () => System.import("@test/mfe-angular-app"),
-//   activeWhen: ["/ng"],
-// });
+registerApplication({
+  name: "@test/mfe-angular-app",
+  app: () => System.import("@test/mfe-angular-app"),
+  activeWhen: ["/angular"],
+});
 
 start({
   urlRerouteOnly: true,
